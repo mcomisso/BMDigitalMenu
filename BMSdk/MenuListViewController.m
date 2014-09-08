8e9c94f1bb5f355cd21f5d979ae8906f8eb616e6
Matteo Comisso <teomatteo89@gmail.com>
2014-09-08 18:54:58 +0200

    Cleaner code, methods for colors. Segue for PDF

--- BMSdk/MenuListViewController.m
+++ BMSdk/MenuListViewController.m
@@ -8,6 +8,8 @@
 
 #import "MenuListViewController.h"
 #import "MenuListCell.h"
+#import "NoImageTableViewCell.h"
+
 #import "BMDataManager.h"
 #import "RecipeDetailViewController.h"
 
@@ -35,6 +37,13 @@
 
 @implementation MenuListViewController
 
+#pragma mark - Color Utils
+-(UIColor *)BMDarkValueColor
+{
+    return [UIColor colorWithRed:0.12 green:0.12 blue:0.12 alpha:1];
+}
+
+#pragma mark - Initialization View methods
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -50,18 +59,10 @@
     // Do any additional setup after loading the view.
 
     self.dataManager = [BMDataManager sharedInstance];
+
+    [self setPreferredToolbar];
     
-    //Scroll to top gesture
-    self.tableView.scrollsToTop = YES;
-    self.title = [self.category uppercaseString];
-    [self.navigationController setNavigationBarHidden:NO animated:YES];
-    self.navigationController.navigationBar.translucent = NO;
-    self.navigationController.navigationBar.tintColor = [UIColor colorWithRed:0.12 green:0.12 blue:0.12 alpha:1];
-    
-    self.view.backgroundColor = [UIColor colorWithRed:0.12 green:0.12 blue:0.12 alpha:1];
-    
-    self.navigationItem.hidesBackButton = YES;
-    [self setBackGesture];
+//    [self setBackGesture];
     
     [self loadRecipesForCategory];
 }
@@ -69,8 +70,22 @@
 -(void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-    
     [self.navigationController setToolbarHidden:YES animated:NO];
+}
+
+-(void)setPreferredToolbar
+{
+    self.tableView.scrollsToTop = YES;
+    self.title = [self.category uppercaseString];
+    [self.navigationController setNavigationBarHidden:NO animated:YES];
+
+    self.navigationController.navigationBar.translucent = NO;
+    self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
+    [self.navigationController.navigationBar setBarTintColor:[self BMDarkValueColor]];
+
+    self.view.backgroundColor = [UIColor colorWithRed:0.12 green:0.12 blue:0.12 alpha:1];
+    
+    self.navigationItem.hidesBackButton = NO;
 }
 
 -(void)setBackGesture
@@ -99,63 +114,89 @@
     // Dispose of any resources that can be recreated.
 }
 
-
 #pragma mark - Table View methods
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *cellIdentifier = @"cellIdentifier";
+    static NSString *whiteCellIdentifier = @"whiteCellIdentifier";
 
     NSDictionary *recipe = [self.recipesInCategory objectAtIndex:indexPath.row];
-    
-    MenuListCell *cell = [self.tableView dequeueReusableCellWithIdentifier:cellIdentifier];
-    cell.selectionStyle = UITableViewCellSelectionStyleNone;
-    
-    cell.recipeId = [recipe objectForKey:@"ricetta_id"];
-    
-    UILabel *recipeName = (UILabel *)[cell viewWithTag:111];
-    recipeName.text = [[recipe objectForKey:@"nome"]uppercaseString];
-    
-    UILabel *recipePrice = (UILabel *)[cell viewWithTag:112];
-    
-    recipePrice.text = [[@"Prezzo: " stringByAppendingString:[recipe objectForKey:@"prezzo"]]stringByAppendingString:@"€"];
-    
-    AXRatingView *thisratingView = [[AXRatingView alloc]initWithFrame:CGRectMake(13, 2, 70, cell.rateViewContainer.frame.size.height)];
-    thisratingView.value = 4.f;
-    thisratingView.tag = 114;
-    thisratingView.numberOfStar = 5;
-    thisratingView.baseColor = [UIColor colorWithRed:0.12 green:0.12 blue:0.12 alpha:1];
-    thisratingView.highlightColor = [UIColor whiteColor];
-    
-    thisratingView.userInteractionEnabled = NO;
-    thisratingView.stepInterval = 1.f;
+    //IF RECIPE HAS IMAGE
+    if ([[recipe objectForKey:@"immagine"]isEqualToString:@"nil"]) {
+        NoImageTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:whiteCellIdentifier];
+        //Setup Cell
+        cell.recipeId = [recipe objectForKey:@"ricetta_id"];
+        UILabel *recipeName = (UILabel *)[cell viewWithTag:200];
+        recipeName.text = [[recipe objectForKey:@"nome"]uppercaseString];
 
-    [cell.rateViewContainer addSubview:thisratingView];
+        UILabel *recipePrice = (UILabel *)[cell viewWithTag:201];
+        recipePrice.text = [[@"Prezzo: " stringByAppendingString:[recipe objectForKey:@"prezzo"]]stringByAppendingString:@"€"];
 
-    cell.recipeImageUrl = [recipe objectForKey:@"immagine"];
-    cell.imageView.contentMode = UIViewContentModeScaleAspectFit;
-    cell.imageView.clipsToBounds = YES;
+        return cell;
+    }
+    else
+    {
+        MenuListCell *cell = [self.tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        
+        cell.recipeId = [recipe objectForKey:@"ricetta_id"];
+        
+        UILabel *recipeName = (UILabel *)[cell viewWithTag:111];
+        recipeName.text = [[recipe objectForKey:@"nome"]uppercaseString];
+        
+        UILabel *recipePrice = (UILabel *)[cell viewWithTag:112];
+        
+        recipePrice.text = [[@"Prezzo: " stringByAppendingString:[recipe objectForKey:@"prezzo"]]stringByAppendingString:@"€"];
+        
+        AXRatingView *thisratingView = [[AXRatingView alloc]initWithFrame:CGRectMake(13, 2, 70, cell.rateViewContainer.frame.size.height)];
+        thisratingView.value = 4.f;
+        thisratingView.tag = 114;
+        thisratingView.numberOfStar = 5;
+        thisratingView.baseColor = [self BMDarkValueColor];
+        thisratingView.highlightColor = [UIColor whiteColor];
+        
+        thisratingView.userInteractionEnabled = NO;
+        thisratingView.stepInterval = 1.f;
+        
+        [cell.rateViewContainer addSubview:thisratingView];
+        
+        cell.recipeImageUrl = [recipe objectForKey:@"immagine"];
+        cell.imageView.contentMode = UIViewContentModeScaleAspectFit;
+        cell.imageView.clipsToBounds = YES;
 
-
-    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/html"];
-
-    thisratingView.value = [self.dataManager requestRatingForRecipe:cell.recipeId];
-    
-    [manager GET:[BMRATEAPI stringByAppendingString:cell.recipeId]
-      parameters:nil
-         success:^(AFHTTPRequestOperation *operation, id responseObject) {
-             int value = [[responseObject objectForKey:@"media"]intValue];
-
-             //Update rating and load from data manager
-             [self.dataManager saveRatingValue:[NSNumber numberWithInt:value] forRecipe:cell.recipeId];
-         }
-         failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-             NSLog(@"Error while giving rate to cells: %@ %@", [error localizedDescription], [error localizedFailureReason]);
-             
-         }];
-    
-    return cell;
+        NSString *downloadString = [BMIMAGEAPI stringByAppendingString:cell.recipeImageUrl];
+        
+        UIImageView *recipeImageView = (UIImageView *)[cell viewWithTag:110];
+        recipeImageView.clipsToBounds = YES;
+        [recipeImageView  sd_setImageWithURL:[[NSURL alloc]initWithString:downloadString]
+                            placeholderImage:[self imageColoredGenerator]
+                                   completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
+                                       if (error) {
+                                           NSLog(@"Error! %@ %@", [error localizedDescription], [error localizedFailureReason]);
+                                       }
+                                   }];
+        
+        AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+        manager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/html"];
+        
+        thisratingView.value = [self.dataManager requestRatingForRecipe:cell.recipeId];
+        
+        [manager GET:[BMRATEAPI stringByAppendingString:cell.recipeId]
+          parameters:nil
+             success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                 int value = [[responseObject objectForKey:@"media"]intValue];
+                 
+                 //Update rating and load from data manager
+                 [self.dataManager saveRatingValue:[NSNumber numberWithInt:value] forRecipe:cell.recipeId];
+             }
+             failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                 NSLog(@"Error while giving rate to cells: %@ %@", [error localizedDescription], [error localizedFailureReason]);
+                 
+             }];
+        
+        return cell;
+    }
 }
 
 -(UIImage *)imageColoredGenerator
@@ -170,6 +211,7 @@
     return image;
 }
 
+/*
 -(void)tableView:(UITableView *)tableView willDisplayCell:(MenuListCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UIImageView *recipeImageView = (UIImageView *)[cell viewWithTag:110];
@@ -193,7 +235,7 @@
         cell.recipeTitle.frame = CGRectMake(10, 10, 320, 45);
         cell.recipePrice.frame = CGRectMake(10, 91, 132, 21);
     }
-}
+}*/
 
 /* Deve ritornare il count degli elemeti padre nell NSDictionary */
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
@@ -209,7 +251,6 @@
 
 #pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
     if ([[segue identifier] isEqualToString:@"details"]) {

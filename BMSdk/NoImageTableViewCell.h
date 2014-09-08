bf220ed203473a16b0a7bcfdc9727995c2d0128c
Matteo Comisso <teomatteo89@gmail.com>
2014-09-08 18:53:36 +0200

    Secondary class without images for list menu items.

--- BMSdk/NoImageTableViewCell.h
+++ BMSdk/NoImageTableViewCell.h
@@ -7,7 +7,16 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "AXRatingView.h"
 
 @interface NoImageTableViewCell : UITableViewCell
+
+@property (strong, nonatomic) IBOutlet UILabel *recipeTitle;
+@property (strong, nonatomic) IBOutlet UILabel *recipePrice;
+@property (strong, nonatomic) IBOutlet UIView *rateViewContainer;
+@property (strong, nonatomic) AXRatingView *ratingView;
+
+@property (strong, nonatomic) NSString *recipeId;
+
 
 @end

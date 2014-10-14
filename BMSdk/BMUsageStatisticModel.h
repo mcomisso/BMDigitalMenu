a6a3211dea20b343d98c866dceb364b6a4238980
Matteo Comisso <teomatteo89@gmail.com>
2014-10-14 18:23:56 +0200

    Creata classe per salvataggio momentaneo

---
+++ BMSdk/BMUsageStatisticModel.h
@@ -0,0 +1,13 @@
+//
+//  BMUsageStatisticModel.h
+//  BMSdk
+//
+//  Created by Matteo Comisso on 14/10/14.
+//  Copyright (c) 2014 Blue-Mate. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BMUsageStatisticModel : NSObject
+
+@end
---
+++ BMSdk/BMUsageStatisticModel.m
@@ -0,0 +1,13 @@
+//
+//  BMUsageStatisticModel.m
+//  BMSdk
+//
+//  Created by Matteo Comisso on 14/10/14.
+//  Copyright (c) 2014 Blue-Mate. All rights reserved.
+//
+
+#import "BMUsageStatisticModel.h"
+
+@implementation BMUsageStatisticModel
+
+@end

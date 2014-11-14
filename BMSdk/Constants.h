a8d97b7d5d19d74d6ad740393f2bdb4a8c1daaa7
Matteo Comisso <teomatteo89@gmail.com>
2014-11-14 19:12:52 +0100

    API URLS inserted in Constants.h

--- BMSdk/Constants.h
+++ BMSdk/Constants.h
@@ -19,3 +19,11 @@
 #define IS_OS_8_OR_LATER    ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
 
 #endif
+
+// Definition Bluemate Api
+#define BMAPI_RECIPES_FROM_MAJ_MIN @"https://bmbackend-misiedo.appspot.com/api/recipes/majmin/"
+#define BMAPI_DAYMENU_FROM_MAJ_MIN @"https://bmbackend-misiedo.appspot.com/api/daymenu/majmin/"
+
+#define BMAPI_RECIPE_DETAILS_FROM_SLUG @"https://bmbackend-misiedo.appspot.com/api/recipe/"
+#define BMAPI_COMMENTS_FOR_RECIPE_SLUG @"https://bmbackend-misiedo.appspot.com/api/comments/recipe/"
+#define BMAPI_RATING_FOR_RECIPE_SLUG @"https://bmbackend-misiedo.appspot.com/api/rating/recipe/"

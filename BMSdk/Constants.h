c3d628902f4c46b40b5cb2416952419eae58cf95
Matteo Comisso <teomatteo89@gmail.com>
2014-12-29 20:58:52 +0100

    Debug-only NSLog

--- BMSdk/Constants.h
+++ BMSdk/Constants.h
@@ -34,3 +34,12 @@
 
 #define BMLocalizedString(key, comment) \
 [[NSBundle bundleWithURL:[[NSBundle mainBundle] URLForResource:@"BMSdk" withExtension:@"bundle"]] localizedStringForKey:(key) value:@"" table:nil]
+
+#ifdef DEBUG
+#   define DLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#else
+#   define DLog(...)
+#endif
+
+// ALog always displays output regardless of the DEBUG setting
+#define ALog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
